59b42f162bc46aecd7e25cae518497ff751b678e
ANTON SILICH <ctykctykreg@gmail.com>
2021-11-08 09:56:25 +0300

    Update

--- terraform/install.sh
+++ terraform/install.sh
@@ -4,3 +4,5 @@ apt upgrade -y
 apt install software-properties-common -y
 add apt-repository ppa:deadsnakes/ppa -y
 apt install python3.9 python3-pip -y
+pip install -r requirements.txt
+curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
